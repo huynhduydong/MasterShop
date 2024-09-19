cd7f84a5ca19a42eeef7731ff5d3575ca0dbd47b
huynhduydong <hddongqn@gmail.com>
2024-09-20 03:54:20 +0700

    feat: add OAuth2 security

--- LavenShopBackend/src/main/java/com/dong/controller/AuthController.java
+++ LavenShopBackend/src/main/java/com/dong/controller/AuthController.java
@@ -8,10 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
