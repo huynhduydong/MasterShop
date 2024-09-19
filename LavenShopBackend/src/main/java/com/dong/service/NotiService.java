8d93adea49309d175f4538cf4fa9f71fff179eba
huynhduydong <hddongqn@gmail.com>
2024-09-20 02:08:17 +0700

    feat: add notification

---
+++ LavenShopBackend/src/main/java/com/dong/service/NotiService.java
@@ -0,0 +1,16 @@
+package com.dong.service;
+
+import com.dong.dto.model.NotiDto;
+import com.dong.dto.response.NotiResponseDto;
+import com.dong.dto.response.ObjectResponse;
+
+public interface NotiService {
+    NotiDto getNotiWithProduct(long productId);
+    ObjectResponse<NotiDto> getAllNotifications(int pageNo, int pageSize, String sortBy, String sortDir);
+    NotiDto getNotificationById(long id);
+    NotiDto createNotification(NotiResponseDto notiDto);
+    NotiDto updateNotification(long notiId, NotiResponseDto notiDto);
+    void deleteNotification(long id);
+    ObjectResponse<NotiDto> searchNoti(String title, int pageNo, int pageSize, String sortBy, String sortDir);
+
+}
